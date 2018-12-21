f2b3785254ce383c1ac153a0f47bd283b43957ec
Fred Hebert <mononcqc@ferd.ca>
2018-12-20 20:22:00 -0500

    Adding basic tests so build passes

---
+++ test/boot_SUITE.erl
@@ -0,0 +1,13 @@
+%%% @doc Check that the app boots and stops fine
+-module(boot_SUITE).
+-include_lib("stdlib/include/assert.hrl").
+-include_lib("common_test/include/ct.hrl").
+-compile(export_all).
+
+all() ->
+    [start_stop].
+
+start_stop(_Config) ->
+    {ok, Apps} = application:ensure_all_started(revault),
+    [ok = application:stop(A) || A <- Apps],
+    ok.
